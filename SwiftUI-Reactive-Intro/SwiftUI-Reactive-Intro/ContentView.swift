3535bd72851400c596c3edc9f04795e57ab95729
TarekJradi <jradi3@gmail.com>
2020-02-17 21:34:57 +0000

    Created initial Content View and some fields added

--- SwiftUI-Reactive-Intro/SwiftUI-Reactive-Intro/ContentView.swift
+++ SwiftUI-Reactive-Intro/SwiftUI-Reactive-Intro/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    
+    @State var firstName = ""
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                VStack {
+                    VStack {
+                        Group {
+                            TextField("First Name", text: $firstName).padding(12)
+                        }.background(Color.white)
+                        TextField("First Name", text: $firstName)
+                        Button(action: {
+                            
+                        }) {
+                            Text("Create User")
+                        }.background(Color.gray)
+                    }
+                }
+                List {
+                    Text("Empty Row")
+                }
+            }
+        }.navigationBarTitle(Text("Credit Card Form"))
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
