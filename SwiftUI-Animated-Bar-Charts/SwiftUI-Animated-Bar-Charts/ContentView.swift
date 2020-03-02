7b039236e51419b552878185106a2fecab28e70c
TarekJradi <jradi3@gmail.com>
2020-03-02 23:03:10 +0000

    [SwiftUI-Animated-Bar-Charts] Created of BarView component and added animation for Bars.

--- SwiftUI-Animated-Bar-Charts/SwiftUI-Animated-Bar-Charts/ContentView.swift
+++ SwiftUI-Animated-Bar-Charts/SwiftUI-Animated-Bar-Charts/ContentView.swift
@@ -11,15 +11,18 @@ import SwiftUI
 struct ContentView: View {
     
     @State var pickerSelectedItem = 0
+    @State var dataPoints: [[CGFloat]] = [
+        [50, 100, 150],
+        [150, 50, 100],
+        [50, 70, 30]
+    ]
     
     var body: some View {
-        
         
         ZStack {
             Color.green.edgesIgnoringSafeArea(.all)
             
             VStack {
-                
                 Text("Hello, World!")
                      .font(.system(size: 34))
                      .fontWeight(.heavy)
@@ -33,88 +36,42 @@ struct ContentView: View {
                 .padding(.horizontal, 24)
 
                 HStack (spacing: 16) {
-                    
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("Su").padding(.top, 8)
-                    }
-
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("M").padding(.top, 8)
-                    }
-
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("T").padding(.top, 8)
-                    }
-
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("W").padding(.top, 8)
-                    }
-
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("TH").padding(.top, 8)
-                    }
-
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("F").padding(.top, 8)
-                    }
-                    
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                             Capsule().frame(width: 30, height:200)
-                                 .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
-                             Capsule().frame(width: 30, height:100)
-                                 .foregroundColor(.white)
-
-                         }
-                        Text("Sa").padding(.top, 8)
-                    }
-                }.padding(.top, 24)
+                    BarView(value: dataPoints[pickerSelectedItem][0])
+                    BarView(value: dataPoints[pickerSelectedItem][1])
+                    BarView(value: dataPoints[pickerSelectedItem][2])
+                }
+                .padding(.top, 24)
+                .animation(.default)
             }
          }
     }
 }
+
+/// Presents a BarView.
+///
+///     The BarView use a capsule function, that is
+///     equivalent to a rounded rectangle where the corner radius is chosen in UIView.
+///
+/// - Parameters:
+///     - value: The `Binding` value of the BarView
+///
+struct BarView: View {
+
+    var value: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            ZStack (alignment: .bottom) {
+                 Capsule().frame(width: 30, height:200)
+                     .foregroundColor(Color(#colorLiteral(red: 0.3038644195, green: 0.9624463916, blue: 0.6871674657, alpha: 1)))
+                 Capsule().frame(width: 30, height:value)
+                     .foregroundColor(.white)
+             }
+            Text("Sa").padding(.top, 8)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
