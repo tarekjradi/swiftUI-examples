abc528a53bd806aac9bba61b24a296b54b24365c
Tarek Abdala <jradi3@gmail.com>
2020-03-09 15:17:27 +0000

    [SwiftUI-CalculatorView] CalculatorButtons with Enumeration and Switches

--- SwiftUI-Calculator/SwiftUI-Calculator/CalculatorView.swift
+++ SwiftUI-Calculator/SwiftUI-Calculator/CalculatorView.swift
@@ -8,13 +8,58 @@
 
 import SwiftUI
 
+enum CalculatorButton: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide, dot
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+            case .zero: return "0"
+            case .one: return "1"
+            case .two: return "2"
+            case .three: return "3"
+            case .four: return "4"
+            case .five: return "5"
+            case .six: return "6"
+            case .seven: return "7"
+            case .eight: return "8"
+            case .nine: return "9"
+            case .plus: return "+"
+            case .minus: return "-"
+            case .multiply: return "X"
+            case .divide: return "÷"
+            case .plusMinus: return "±"
+            case .percent: return "%"
+            case .dot: return "."
+            case .equals: return "="
+        default:
+            return "AC"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .dot:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return .orange
+        }
+    }
+}
+
 struct CalculatorView: View {
     
-    let buttons = [
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", ".", "="]
+    @State var value = ""
+
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equals],
     ]
     
     var body: some View {
@@ -24,7 +69,7 @@ struct CalculatorView: View {
                 
                 HStack {
                     Spacer()
-                    Text("42").foregroundColor(.white)
+                    Text(value).foregroundColor(.white)
                         .font(.system(size: 64))
 
                 }.padding()
@@ -32,12 +77,21 @@ struct CalculatorView: View {
                 ForEach(buttons, id: \.self) { row in
                     HStack (spacing: 12) {
                         ForEach(row, id: \.self) { button in
-                            Text(button)
-                                .font(.system(size: 32))
-                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
-                                .foregroundColor(.white)
-                                .background(Color.yellow)
-                                .cornerRadius(self.buttonWidth())
+                            Button(action: {
+                                switch button {
+                                    case .ac:
+                                        self.value = ""
+                                    default:
+                                        self.value = "\(self.value)\(button.title)"
+                                }
+                            }) {
+                                Text(button.title)
+                                    .font(.system(size: 32))
+                                    .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                                    .foregroundColor(.white)
+                                    .background(button.backgroundColor)
+                                    .cornerRadius(self.buttonWidth(button: button))
+                            }
                         }
                     }
                 }
@@ -46,7 +100,12 @@ struct CalculatorView: View {
         }
     }
     
-    func buttonWidth() -> CGFloat {
+    func buttonWidth(button: CalculatorButton) -> CGFloat {
+        
+        if button == .zero {
+            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        }
+        
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
