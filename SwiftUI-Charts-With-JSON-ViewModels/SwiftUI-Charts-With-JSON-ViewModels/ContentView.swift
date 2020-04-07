dda539394bdc08526f9a66f5b03ca735ecc28626
TarekJradi <jradi3@gmail.com>
2020-04-07 10:07:02 +0100

    [SwiftUI-Charts-With-JSON-ViewModels] create project

---
+++ SwiftUI-Charts-With-JSON-ViewModels/SwiftUI-Charts-With-JSON-ViewModels/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Charts-With-JSON-ViewModels
+//
+//  Created by Tarek Abdala on 06/04/2020.
+//  Copyright © 2020 Tarek Abdala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
