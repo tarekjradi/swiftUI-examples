41be16e5ec7a267349dfee7b2ae01a612b364b25
TarekJradi <jradi3@gmail.com>
2020-04-27 21:51:10 +0100

    [SwiftUI-Charts-With-JSON-ViewModels] added timeseries json file and HStack width custom

--- SwiftUI-Charts-With-JSON-ViewModels/SwiftUI-Charts-With-JSON-ViewModels/ContentView.swift
+++ SwiftUI-Charts-With-JSON-ViewModels/SwiftUI-Charts-With-JSON-ViewModels/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct TimeSeries: Decodable {
     let Thailand : [DayData]
     let US:  [DayData]
+    let Italy: [DayData]
+    let Hungary: [DayData]
 }
 
 struct DayData: Decodable, Hashable, Identifiable {
@@ -27,7 +29,7 @@ class ChartsViewModel: ObservableObject {
     var max = 0
     
     init() {
-        let urlString = "https://pomber.github.io/covid19/timeseries.json"
+        let urlString = "https://github.com/tarekjradi/SwiftUI-Patterns/timeseries.json"
         guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, resp, error) in
             guard let data = data else { return }
@@ -35,7 +37,7 @@ class ChartsViewModel: ObservableObject {
                 let timeSeries = try JSONDecoder().decode(TimeSeries.self, from: data)
                 
                 DispatchQueue.main.sync {
-                    self.dataSet = timeSeries.US.filter({ $0.deaths > 0})
+                    self.dataSet = timeSeries.Italy.filter({ $0.deaths > 0})
                     
                     self.max = self.dataSet.max(by: { (day1, day2) -> Bool in
                         return day2.deaths > day1.deaths
@@ -63,7 +65,7 @@ struct ContentView: View {
                         ForEach(viewModel.dataSet, id: \.self) { day in
                             HStack {
                                 Spacer()
-                            }.frame(width: 10, height: (CGFloat(day.deaths) / CGFloat(self.viewModel.max)) * 200)
+                            }.frame(width: 2, height: (CGFloat(day.deaths) / CGFloat(self.viewModel.max)) * 200)
                             .background(Color.red)
                         }
                     }
