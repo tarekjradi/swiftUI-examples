e6ee857a81f587a7b497b58ff417e4f04d836697
Tarek <jradi3@gmail.com>
2024-09-08 15:55:17 +0100

    [SwiftUI-Navigation] create project

---
+++ SwiftUI-Navigation/SwiftUI-Navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Navigation
+//
+//  Created by Tarek Jradi on 08/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Navigation/SwiftUI-Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI-Navigation
+//
+//  Created by Tarek Jradi on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
